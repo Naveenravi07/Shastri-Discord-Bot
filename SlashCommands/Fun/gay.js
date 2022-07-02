5060ffaa175641f99095c9e3e67d5a2ea232c51a
Shastri Gaming <gamerkidnaveen@gmail.com>
2022-07-02 04:09:52 -0700

    Slash Command Base With Say Command

---
+++ SlashCommands/Fun/gay.js
@@ -0,0 +1,36 @@
+const discord = require('discord.js');
+const { Clinet, Message, MessageEmbed, CommandInteraction } = require('discord.js');
+module.exports = {
+  name: 'gay',
+  description: 'Shows the Chance Of Being A Gay',
+  options: [
+    {
+      name: 'mention',
+      description: 'Mention a user',
+      type: 'USER',
+      required: false,
+    },
+  ],
+  async run(client, interaction, args) {
+    const gayguy = interaction.options.getUser('mention');
+    if (!gayguy) {
+      const gay = Math.floor(Math.random() * 101);
+      const gayembed = new MessageEmbed()
+        .setTitle(`Gay Test`)
+        .setDescription(`<@${interaction.member.id}> Is Literally ${gay} % Gay  `)
+        .setColor('YELLOW')
+        .setTimestamp()
+        .setThumbnail(interaction.member.displayAvatarURL({ dynamic: true }));
+      return interaction.reply({ embeds: [gayembed], ephemeral: false });
+    } else {
+      const gay = Math.floor(Math.random() * 101);
+      const gayembed = new MessageEmbed()
+        .setTitle(`Gay Test`)
+        .setDescription(`<@${gayguy.id}> Is Literally ${gay} % Gay  `)
+        .setColor('YELLOW')
+        .setTimestamp()
+        .setThumbnail(gayguy.displayAvatarURL({ format: 'png' }));
+      return interaction.reply({ embeds: [gayembed], ephemeral: false });
+    }
+  },
+};
---
+++ SlashCommands/Moderation/say.js
@@ -0,0 +1,99 @@
+const discord = require('discord.js');
+const { Permissions, MessageEmbed } = require('discord.js');
+const { Client, CommandInteraction } = require('discord.js');
+
+module.exports = {
+  name: 'say',
+  description: 'announce',
+  type: 'CHAT_INPUT',
+  options: [
+    {
+      name: 'message',
+      description: 'Type the message that you want to  announce',
+      type: 'STRING',
+      required: true,
+    },
+    {
+      name: 'channel',
+      description: 'Select the channel for announcement',
+      type: 'CHANNEL',
+      required: true,
+    },
+    {
+      name: 'role',
+      description: 'Provide the roles that you  want to ping',
+      type: 'ROLE',
+      required: false,
+    },
+    {
+      name: 'image-url',
+      description: 'Provide the image url',
+      type: 'STRING',
+      required: false,
+    },
+  ],
+
+  async run(client, interaction, args) {
+    /**
+     *    * @param {Client} client
+     * @param {CommandInteraction} interaction
+     */
+
+    if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
+      interaction.reply(`You Dont Have Permissions To Announce Message`);
+    } else {
+      let { options, user } = interaction;
+
+      let channel = options.getChannel('channel');
+      let text = options.getString('message');
+      let role = options.getRole('role');
+      let imageurl = options.getString('image-url');
+
+      if (imageurl) {
+        if (!imageurl.startsWith('https://media.discordapp.net/attachments/')) {
+          let invalidlinkembedd = new MessageEmbed();
+          console.log(user.tag);
+          invalidlinkembedd
+            .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL({ dynamic: true }) })
+            .setColor('DARK_AQUA')
+            .setDescription(
+              `Image url starting with \`https://media.discordapp.net/attachments/\` are only accepted 
+               '\n' Simply sent your image  to any discord channel then copy the link and paste it here `
+            );
+
+          interaction.reply({ embeds: [invalidlinkembedd], ephemeral: true });
+        }
+      }
+
+      const image = imageurl;
+      const sayembed = new MessageEmbed()
+        .setColor('BLUE')
+        .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL({ dynamic: true }) })
+        .setTitle('NEW ANNOUNCEMENT')
+        .setDescription(text)
+        .setImage(image)
+        .setTimestamp()
+        .setFooter({ text: 'Announced By ShastriClone' });
+
+      if (role) {
+        channel.send({ content: `${role}`, embeds: [sayembed] }).then((msg) => {
+          msg.react(`✅`);
+          msg.react(`⛔`);
+          return interaction.reply({
+            embeds: [new MessageEmbed().setColor('DARKER_GREY').setDescription(`✅ Your Announcement is now live in ${channel}`)],
+            ephemeral: true,
+          });
+        });
+      } else {
+        channel.send({ embeds: [sayembed] }).then((msg) => {
+          msg.react(`✅`);
+          msg.react(`⛔`);
+          return interaction.reply({
+            embeds: [new MessageEmbed().setColor('DARKER_GREY').setDescription(`✅ Your Announcement is now live in ${channel}`)],
+            ephemeral: true,
+          });
+        });
+      }
+    }
+  },
+};
